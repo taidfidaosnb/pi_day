f8655c47a8f6246c0b25e301d05be07e9994caab
in-zet <74442374+in-zet@users.noreply.github.com>
2023-03-13 23:43:31 +0900

    Update simple_gui.py

--- simple_gui.py
+++ simple_gui.py
@@ -2,44 +2,64 @@ from tkinter import *
 from tkinter import ttk
 
 
-def main_roop(calc, exceptions: int = 0) -> None:  # tkinter를 이용한 간단한 GUI
+def main_roop(calc, title: str, text_label: str, test: bool = False) -> None:
     """
     Simple Gui that has 1 input line and 1 display line and a button used to run function 'calc'
 
     :param calc: Function that requires 1 str and returns Any
-    :param exceptions: Type int, default 0, runs try-except syntax if not False
+    :param title:
+    :param text_label:
+    :param test: Type bool, default False, runs try-except syntax if not False
     :return: None
     """
 
     global e2, e3
 
-    def run(calc, exceptions):
+    def run(dcalc, dtest):
         n = e2.get()
         e3.delete(0, len(e3.get()))
-        if exceptions:
+        if dtest:
             try:
-                e3.insert(0, str(calc(n)))
+                e3.insert(0, str(dcalc(n)))
             except Exception as e:
                 e3.insert(0, f"ERROR: {e}")
         else:
-            e3.insert(0, calc(n))
+            e3.insert(0, dcalc(n))
+
+    def clear():
+        e2.delete(0, len(e2.get()))
+        e3.delete(0, len(e3.get()))
+
+    font = "Arial 24"
 
     window = Tk()  # 창 생성
-    window.geometry("270x170")  # 크기
+    window.geometry("770x314")  # 크기
+    window.title(title)
+    window.resizable(False, False)
 
-    l2 = Label(window, text="n:")
-    l3 = Label(window, text="ans:")
-    l2.place(x=10, y=40)
-    l3.place(x=10, y=70)
+    l1 = Label(window, text=text_label, font=font)
+    l1.place(x=240, y=10)
 
-    e2 = ttk.Entry(window, width=30)
-    e3 = ttk.Entry(window, width=30)
-    e2.place(x=50, y=40)
-    e3.place(x=50, y=70)
+    l2 = Label(window, text="n:", font=font)
+    l3 = Label(window, text="ans:", font=font)
 
-    b1 = ttk.Button(window, text="RUN", command=lambda: run(calc, exceptions))
-    b2 = ttk.Button(window, text="DEL", command=lambda: e2.delete(0, len(e2.get())))
-    b1.place(x=40, y=100)
-    b2.place(x=130, y=100)
+    l2.place(x=55, y=75)
+    l3.place(x=35, y=140)
+
+    e2 = ttk.Entry(window, width=35, font=font)
+    e3 = ttk.Entry(window, width=35, font=font)
+
+    e2.place(x=115, y=75)
+    e3.place(x=115, y=140)
+
+    b1 = ttk.Button(window, text="RUN", width=10, command=lambda: run(calc, test))
+    b2 = ttk.Button(window, text="DEL", width=10, command=clear)
+
+    b1.place(x=115, y=200)
+    b2.place(x=210, y=200)
 
     window.mainloop()
+
+
+if __name__ == "__main__":
+    main_roop(lambda x: int(x) ** 2)
